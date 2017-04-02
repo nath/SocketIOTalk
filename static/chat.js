d2b8c3462dbd5ab23df24368dc38395baa42fc68
Nath Tumlin <ntumlin@gmail.com>
2017-04-02 18:46:22 -0500

    Added styling

--- static/chat.js
+++ static/chat.js
@@ -1,11 +1,13 @@
 var socket = io();
 
-const messageBox = $('#messageBox');
+const inbox = $('#inbox');
 const msgBox = $('#msgBox');
 const name = prompt('What is your name?');
 
-$('form').submit(() => {
-    const msgText = msgBox.val();
+msgBox.select();
+
+function sendMessage() {
+    const msgText = msgBox.val().trim();
     if (msgText === '') {
         return;
     }
@@ -21,11 +23,45 @@ $('form').submit(() => {
     msgBox.val('');
 
     return false;
+}
+
+$('form').submit(sendMessage);
+
+msgBox.keydown((e) => {
+    if (e.keyCode == 13 && !e.shiftKey) {
+        e.preventDefault();
+        sendMessage();
+    }
 });
 
+//escape html taken from underscore js
+
+let htmlEscapes = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+  '"': '&quot;',
+  "'": '&#x27;',
+  '/': '&#x2F;'
+};
+
+var htmlEscaper = /[&<>"'\/]/g;
+
+function escape(string) {
+  return ('' + string).replace(htmlEscaper, function(match) {
+    return htmlEscapes[match];
+  });
+};
 
 function addMessage(msg) {
-    messageBox.append(`<div class="message">${msg.name}: ${msg.text}</div>`);
+    let shouldScroll = inbox[0].scrollHeight - inbox.scrollTop() - inbox.outerHeight() < 1;
+
+    msg.text = escape(msg.text.trim()).replace(/\n/, '<br>');
+    inbox.append(`<div class="message"><span class="name">${msg.name}:</span> <span class="text">${msg.text}</span></div>`);
+
+    if (shouldScroll) {
+        inbox.scrollTop(inbox[0].scrollHeight);
+    }
 }
 
 socket.on('message', (msg) => {
