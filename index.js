e4e185970fb1bd30af1575af59016359b663d04b
Nath Tumlin <ntumlin@gmail.com>
2017-04-02 16:39:31 -0500

    Created static directory

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+app.use(express.static('./static'));
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
