64ade823a4ad0cb6d5a4280757e17374a3466ca5
Nath Tumlin <ntumlin@gmail.com>
2017-04-02 16:49:29 -0500

    Added socketio

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.use(express.static('./static'));
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(3000, () => {
     console.log('Listening on port 3000');
 });
